9f8c35c1b4fcd507dc9386ef50e6ac25d6b81464
steffahv <tefavv22@gmail.com>
2025-01-17 20:20:19 -0500

    adding file

---
+++ stopwords.jl
@@ -0,0 +1,19 @@
+module StopWords
+
+export get_stop_words
+
+function get_stop_words()
+    return [
+        "the", "to", "of", "and", "i", "a", "in", "he", "was", "that", "it", "you", "has", "his", "had", "as", 
+        "with", "her", "for", "my", "at", "not", "is", "but", "she", "on", "me", "s", "have", "this", "him", "which", "be", "by", 
+        "said", "from", "one", "all", "no", "what", "an", "who", "so", "if", "there", "been", "when", "were", "would", "t", "we", 
+        "will", "or", "they", "your", "are", "out", "do", "could", "did", "done", "mr", "up", "then", "more", "into", "them", "some", 
+        "about", "can", "before", "after", "only", "very", "any", "than", "like", "am", "their", "upon", "other", "now", "first", 
+        "here", "two", "back", "how", "way", "us", "must", "himself", "over", "never", "should", "well", "little", "own", "say", 
+        "think", "such", "may", "might", "come", "down", "miss", "our", "nothing", "even", "again", "last", "don", "its", "where", 
+        "went", "mrs", "sir", "left", "these", "without", "much", "yes", "old", "still", "too", "yet", "man", "tell", "asked", "good",
+        "found", "just", "go", "thought", "looked", "m", "made", "once", "myself"
+    ]
+end
+
+end
